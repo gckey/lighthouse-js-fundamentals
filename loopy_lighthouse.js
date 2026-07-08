e815982fc3fe63f71322013094d300ee5b369dd3
gckey <abdorehman@gmail.com>
2026-07-07 21:05:03 -0600

    Refractor loopy_lighthouse.js: DRY up multiple checks into single loop with string building

--- loopy_lighthouse.js
+++ loopy_lighthouse.js
@@ -1,14 +1,10 @@
-var x = 100;
-// while loop with a stop condition
-while (x <= 200) {
-  if (x % 3 === 0 && x % 4 === 0) {
-    console.log("LoopyLighthouse");
-  } else if (x % 4 === 0) {
-    console.log("Lighthouse");
-  } else if (x % 3 === 0) {
-    console.log("Loopy");
-  } else {
-    console.log(x);
-  }
-  x = x + 1;
+// Refractor loopy_lighthouse.js using DRY principle.
+
+for (let num = 100; num <= 200; num++) {
+  let output = "";
+  if (num % 3 === 0)
+    output += "Loopy";
+  if (num % 4 === 0)
+    output += "Lighthouse";
+  console.log(output || num);
 }
